a6b21df676e6272ef9e03ddf20a447d4ee40ac02
Виталя <@DESKTOP-NOMCK43>
2024-10-31 11:32:31 +0800

    Добавьте файлы проекта.

---
+++ Laba1.2/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Laba1._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(263, 313);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(264, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "поиск";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(263, 79);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(264, 187);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(263, 287);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(264, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(263, 343);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(264, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(791, 587);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Laba1.2/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba1._2
+{
+    public partial class Form1 : Form
+    {
+        private const string ImagesDirectory = @"C:\Users\Виталя\Desktop\Laba1.2-master\Pictyre"; 
+        private string currentImagePath; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            string query = textBox2.Text;
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                string imageUrl = await GetRandomImageUrl(query);
+                if (!string.IsNullOrEmpty(imageUrl))
+                {
+                    currentImagePath = imageUrl;
+                    pictureBox2.Load(imageUrl);
+                }
+                else
+                {
+                    MessageBox.Show("Изображение не найдено.");
+                }
+            }
+        }
+
+        private async Task<string> GetRandomImageUrl(string query)
+        {
+            try
+            {
+                var searchPattern = $"{query}*.jpg"; 
+                var files = Directory.GetFiles(ImagesDirectory, searchPattern);
+
+                if (files.Length > 0)
+                {
+                    Random rand = new Random();
+                    int randomIndex = rand.Next(files.Length);
+                    return files[randomIndex]; 
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+            return null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(currentImagePath))
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Image files (*.jpg;*.jpeg;*.png)|*.jpg;*.jpeg;*.png";
+                saveFileDialog.FileName = Path.GetFileName(currentImagePath);
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        File.Copy(currentImagePath, saveFileDialog.FileName, true);
+                        MessageBox.Show("Изображение успешно скачано!");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка при скачивании изображения: {ex.Message}");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Сначала выберите изображение для скачивания.");
+            }
+        }
+    }
+}
